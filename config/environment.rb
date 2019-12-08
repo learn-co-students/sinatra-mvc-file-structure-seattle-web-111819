bad03f281cd75f312e9b6d156f27247d74c01beb
4myc <missch3n@gmail.com>
2019-12-08 11:07:54 -0800

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
+#environment.rb connects up all the files in our application to the appropriate gems and to each other
 ENV['SINATRA_ENV'] ||= "development"
 ENV['RACK_ENV'] ||= "development"
 
+#loads Bundler and thus all the gems in our Gemfile, as well as the app directory
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
